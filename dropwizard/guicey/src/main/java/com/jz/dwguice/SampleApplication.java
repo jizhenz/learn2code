7ae997a24e87481de5c0cab7a9cea0844f317794
Jizhen Zhao <jizhen.zhao@gmail.com>
2019-07-15 12:52:27 -0400

    more commit

--- dropwizard/guicey/src/main/java/com/jz/dwguice/SampleApplication.java
+++ dropwizard/guicey/src/main/java/com/jz/dwguice/SampleApplication.java
@@ -5,6 +5,7 @@ import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
+import ru.vyarus.dropwizard.guice.GuiceyOptions;
 
 /**
  * @author Vyacheslav Rusakov
@@ -20,7 +21,11 @@ public class SampleApplication extends Application<Configuration> {
     public void initialize(Bootstrap<Configuration> bootstrap) {
         bootstrap.addBundle(GuiceBundle.builder()
                 .enableAutoConfig(getClass().getPackage().getName())
-                .useWebInstallers()
+                // bridge is required to inject guice service into hk managed sub resource
+                .option(GuiceyOptions.UseHkBridge, true)
+                // make sure service will not be created in both contexts (advanced validation for test)
+                .strictScopeControl()
+//                .useWebInstallers()
                 .modules(new SampleModule())
                 .build());
     }
--- dropwizard/guicey/src/test/groovy/com/jz/dwguice/SampleApplicationTest.groovy
+++ dropwizard/guicey/src/test/groovy/com/jz/dwguice/SampleApplicationTest.groovy
@@ -10,17 +10,34 @@ import spock.lang.Specification
 @UseDropwizardApp(SampleApplication)
 class SampleApplicationTest extends Specification {
 
-    def "Check application startup"() {
+//    def "Check application startup"() {
+//
+//        when: "call resource directly"
+//        new URL("http://localhost:8080/sample/").getText()
+//        then: "not allowed"
+//        def ex = thrown(IOException)
+//        ex.message.startsWith('Server returned HTTP response code: 401 for URL')
+//
+//        when: "call resource correctly"
+//        def res = new URL("http://localhost:8080/sample/?user=me").getText()
+//        then: "allowed"
+//        res
+//    }
 
-        when: "call resource directly"
-        new URL("http://localhost:8080/sample/").getText()
-        then: "not allowed"
-        def ex = thrown(IOException)
-        ex.message.startsWith('Server returned HTTP response code: 401 for URL')
+    def "Check guice sub resource"() {
 
-        when: "call resource correctly"
-        def res = new URL("http://localhost:8080/sample/?user=me").getText()
-        then: "allowed"
-        res
+        when: "call guice managed sub resource"
+        def res = new URL("http://localhost:8080/root/12/guice-sub").getText()
+        then: "correct"
+        res == 'guice root1 12'
     }
+
+    def "Check hk sub resource"() {
+
+        when: "call hk managed sub resource"
+        def res = new URL("http://localhost:8080/root/11/hk-sub").getText()
+        then: "correct"
+        res == 'hk root2 11'
+    }
+
 }
